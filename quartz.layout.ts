a2b2a954377f8ae7b2d85e335ae64c3f771c7c02
triakshal <triakshal@gmail.com>
2025-05-17 15:40:41 -0400

    Quartz sync: May 17, 2025, 3:40 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+      Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'triakshal/triakshal',
+      // from data-repo-id
+      repoId: 'R_kgDOOqp2pg',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDOOqp2ps4CqRks',
+    }
+  }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/triakshal/triakshal",
